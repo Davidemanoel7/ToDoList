2a31b6d56e2f52accedc59d60c0d86b5d9746871
david <david.emanoel.706@ufrn.edu.br>
2023-09-01 20:30:12 -0300

    import myHomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import'./pages/MyHomePage.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,126 +8,9 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context ) {
     return MaterialApp(
-      title: 'To-do List',
+      // title: 'To-do List',
+      debugShowCheckedModeBanner: false,
       home: MyHome(),
-
     );
   }
-}
-
-class MyHome extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return MyHomeState();
-  }
-}
-
-class MyHomeState extends State<MyHome> {
-  
-  final TextEditingController taskController = TextEditingController();
-  final GlobalKey<FormState> _formKey =  GlobalKey<FormState>();
-  final tasks = <String>[];
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        centerTitle: true,
-        title: Text('To Do List'),
-        titleTextStyle: TextStyle(
-          fontWeight: FontWeight.w500,
-          color: Colors.white,
-          fontSize: 20
-        ),
-      ),
-      body: Container(
-        padding: EdgeInsets.fromLTRB(24, 32, 24, 32),
-        child: Column(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-              child: Form(
-              key: _formKey,
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: TextFormField(
-                      controller: taskController,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        color: Colors.black87
-                      ),
-                      decoration: const InputDecoration(
-                        hintText: 'type a new task',
-                        hintStyle: TextStyle(
-                          fontSize: 14,
-                          color: Colors.black38
-                        )
-                      ),
-                      keyboardType: TextInputType.text,
-                      validator: (value) {
-                        if (value!.trim().isEmpty){
-                          return 'Task filed it\'s required';
-                        }
-                        return null;
-                      },
-                    )
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(16, 0, 24, 0),
-                    child: ElevatedButton(
-                      onPressed: (){
-                        if(_formKey.currentState!.validate()) {
-                          setState(() {
-                            tasks.add(taskController.text);
-                          });
-                          taskController.clear();
-                        }
-                      },
-                      child: Text(
-                        'Add',
-                        style:  TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w700,
-                        )  
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue
-                      ),
-                    )
-                  ),
-                ],
-              )
-            ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: tasks.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: ListTile(
-                      title: Text(tasks[index]),
-                      leading: ElevatedButton(
-                        onPressed: (){
-                          setState(() {
-                            tasks.remove(tasks[index]);
-                          });
-                        },
-                        child: Icon(
-                          Icons.remove_circle,
-                          color: Colors.red.shade400,
-                        )
-                      ) ,
-                    ),
-                        
-                  );
-                },
-              ) 
-            ),
-          ],
-        ),
-      ),   
-    );
- } 
 }
